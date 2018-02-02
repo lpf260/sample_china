2544c11f620fcb593f5460dbb306ea050c95ab04
lpf <754476346@qq.com>
2018-02-02 10:06:05 +0000

    Finish user update store index destory actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use App\Http\Requests;
 use Auth;
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     //
     public function create()
     {
@@ -25,7 +32,7 @@ class SessionsController extends Controller
         if(Auth::attempt($credentials, $request->has('remember'))){
             //登录成功
             session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         }else{
             //登陆失败
             session()->flash('danger','很抱歉，您的用户名和密码不匹配');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,27 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show','creat','store','index']
+        ]);
+
+        $this->middleware('guest',[
+            'only'  => ['create']
+        ]);
+    }
+
+    /**
+     * 查看所有用户
+     * @return [type] [description]
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     //
     public function create()
     {
@@ -42,5 +63,54 @@ class UsersController extends Controller
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         //return redirect()->route('profile', ['id'=>1]);
         return redirect()->route('users.show',[$user->id]);
+    }
+
+    public function edit(User $user)
+    {
+        /*        if(Auth::user()->can('update',$user))
+                {
+                    return view('users.edit',compact('user'));
+                }else{
+                    return "mistake";
+                };
+                if(Auth::user()->can('update',$user))
+                {
+                    return view('users.edit',compact('user'));
+                }else{
+                    return "mistake";
+                };
+        */
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+    }
+
+    //修改用户资料
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+            'name'      => 'required|max:50',
+            'password'  => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success','成功删除用户！');
+        return back();
     }
 }
